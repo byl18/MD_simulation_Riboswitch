c2671f5d5ad69cf5717ff00e1f600777fe9f1e4a
byl18 <65812792+byl18@users.noreply.github.com>
2026-08-11 18:05:35 +0900

    Delete preQ1/preQ0 directory

--- preQ1/preQ0/amber2gromacs.py
+++
@@ -1,9 +0,0 @@
-import sys
-import parmed as pmd
-
-amber_name = sys.argv[1]
-gromacs_name = sys.argv[2]
-
-amber = pmd.load_file(f"{amber_name}.prmtop", f"{amber_name}.inpcrd")
-amber.save(f"{gromacs_name}.gro")
-amber.save(f"{gromacs_name}.top")
